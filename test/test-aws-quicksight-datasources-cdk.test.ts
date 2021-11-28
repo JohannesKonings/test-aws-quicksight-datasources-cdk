96356bdfaf981d1a5f77409222e526527eedd6b9
Johannes Konings <johannes.konings@outlook.com>
2021-11-28 11:58:15 +0000

    init

---
+++ test/test-aws-quicksight-datasources-cdk.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as TestAwsQuicksightDatasourcesCdk from '../lib/test-aws-quicksight-datasources-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/test-aws-quicksight-datasources-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestAwsQuicksightDatasourcesCdk.TestAwsQuicksightDatasourcesCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
