ee5887a1c29b03187ba81bad5b079a2610e1794a
Johannes Konings <johannes.konings@outlook.com>
2021-11-28 15:47:44 +0000

    add s3 bucket and example files

--- lib/test-aws-quicksight-datasources-cdk-stack.ts
+++ lib/test-aws-quicksight-datasources-cdk-stack.ts
@@ -1,15 +1,52 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as kms from '@aws-cdk/aws-kms'
+import * as s3 from '@aws-cdk/aws-s3'
+import * as s3Deploy from '@aws-cdk/aws-s3-deployment'
+import * as fs from 'fs';
 
 export class TestAwsQuicksightDatasourcesCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const kmsKey = new kms.Key(this, 'MyKey', {
+      enableKeyRotation: true,
+      pendingWindow: cdk.Duration.days(7),
+    });
+    kmsKey.addAlias('alias/test-aws-quicksight-datasources-cdk');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TestAwsQuicksightDatasourcesCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucketNameDataSource = 'jaykay-s3-quicksight-datasource-bucket'
+
+    const bucketDataSource = new s3.Bucket(this, 's3-datasource-bucket', {
+      bucketName: bucketNameDataSource,
+      encryptionKey: kmsKey,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    })
+
+    const fileNameManifest = 'manifest.json';
+    const fileNameExampleData = 'exampleData.csv';
+
+
+    const manifestBody = {
+      fileLocations: [
+        {
+          URIs: [
+            `s3://${bucketNameDataSource}/${fileNameExampleData}`
+          ]
+        }
+      ],
+      globalUploadSettings: {
+        format: 'CSV',
+        delimiter: ',',
+        containsHeader: 'true'
+      }
+    }
+    fs.writeFileSync('./lib/s3Files/manifest.json', JSON.stringify(manifestBody, null, 4), 'utf8');
+
+    new s3Deploy.BucketDeployment(this, 's3-example-data', {
+      sources: [s3Deploy.Source.asset('./lib/s3Files')],
+      destinationBucket: bucketDataSource,
+    });
+
   }
 }
